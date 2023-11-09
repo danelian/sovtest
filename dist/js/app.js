2e46e2cfcf709dda88ca1e6eacfc085444501624
danelian <141317269+danelian@users.noreply.github.com>
2023-11-09 08:32:37 +0300

    main page block

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
 
+  // -------------------- MAIN GRID ----------------------
+  var mainCards = document.querySelector('.main__cards');
+  var mainCardsArray = mainCards.querySelectorAll('.main-card.one, .main-card.two, .main-card.three, .main-card.four, .main-card.five');
+  mainCardsArray.forEach(function(element) {
+    element.addEventListener('mouseover', function() {
+      mainCards.classList.add(element.classList[1]);
+    });
+    element.addEventListener('mouseout', function() {
+      mainCards.classList.remove(element.classList[1]);
+    });
+  });
+
 });
