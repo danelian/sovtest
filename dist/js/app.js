4fe4d466505f449ae37391cf6f4dac859bddbbe7
danelian <141317269+danelian@users.noreply.github.com>
2023-11-10 04:00:12 +0300

    full page & hero

--- dist/js/app.js
+++ dist/js/app.js
@@ -13,3 +13,39 @@ $(document).ready(function () {
   });
 
 });
+
+
+
+// fullpage customization
+$('#fullpage').fullpage({
+  sectionSelector: '.vertical-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  menu: '#menu',
+
+  afterLoad: function(anchorLink, index) {
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+  afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
+    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(false, 'up');
+    }
+  },
+
+  onSlideLeave: function( anchorLink, index, slideIndex, direction) {
+    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(true, 'up');
+    }
+  } 
+}); 
